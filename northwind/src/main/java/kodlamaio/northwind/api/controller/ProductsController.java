5e1efa6508cd5f8c2a5df4c2ea4913f598c1756d
MustafaColak <m.mustafa.clk@gmail.com>
2021-06-27 23:20:06 +0300

    gün 8

--- northwind/src/main/java/kodlamaio/northwind/api/controller/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controller/ProductsController.java
@@ -21,12 +21,61 @@ public class ProductsController {
     }
 
     @GetMapping("/getall")
-    public DataResult<List<Product>> getAll(){
+    public DataResult<List<Product>> getAll() {
         return productService.getAll();
     }
 
     @PostMapping("/add")
-    public Result add(@RequestBody Product product){
+    public Result add(@RequestBody Product product) {
         return this.productService.add(product);
     }
+
+    @GetMapping("/getbyproductname")
+    public DataResult<Product> getByProductName(@RequestParam String productName) {
+        return productService.getByProductName(productName);
+    }
+
+    @GetMapping("/getByProductNameAndCategoryId")
+    public DataResult<Product> getByProductNameAndCategoryId(@RequestParam String productName, @RequestParam int categoryId) {
+        System.out.println("productName  " + productName);
+        System.out.println("categoryId " + categoryId);
+
+        return productService.getByProductNameAndCategoryId(productName, categoryId);
+    }
+
+    @GetMapping("/getByProductNameOrCategoryId")
+    public DataResult<List<Product>> getByProductNameOrCategoryId(@RequestParam String productName, @RequestParam int categoryId) {
+        return productService.getByProductNameOrCategoryId(productName, categoryId);
+    }
+
+    @GetMapping("/getByProductNameContains")
+    public DataResult<List<Product>> getByProductNameContains(@RequestParam String productName) {
+        return productService.getByProductNameContains(productName);
+    }
+
+    @GetMapping("/getAllByPage")
+    public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+        return productService.getAll(pageNo, pageSize);
+    }
+
+    @GetMapping("/getAllSorted")
+    public DataResult<List<Product>> getAllSorted() {
+        return productService.getAllSorted();
+    }
+
+    @GetMapping("/getByCategoryIdIn")
+    public DataResult<List<Product>> getByCategoryIdIn(@RequestParam List<Integer> categories) {
+        return productService.getByCategoryIdIn(categories);
+    }
+
+    @GetMapping("/getByProductNameStartsWith")
+    public DataResult<List<Product>> getByProductNameStartsWith(@RequestParam String productName) {
+        return productService.getByProductNameStartsWith(productName);
+    }
+
+    @GetMapping("/getByNameAndCategory")
+    public DataResult<List<Product>> getByNameAndCategory(@RequestParam String productName,@RequestParam int categoryId) {
+        return productService.getByNameAndCategory(productName, categoryId);
+    }
+
 }
--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -8,6 +8,9 @@ import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,8 +31,55 @@ public class ProductManager implements ProductService {
     }
 
     @Override
+    public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return new SuccessDataResult<List<Product>>(productDao.findAll(pageable).getContent());
+    }
+
+    @Override
+    public DataResult<List<Product>> getAllSorted() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "productName");
+        return new SuccessDataResult<List<Product>>(productDao.findAll(sort));
+    }
+
+    @Override
     public Result add(Product product) {
         this.productDao.save(product);
         return new SuccessResult("Ürün eklendi");
+    }
+
+    @Override
+    public DataResult<Product> getByProductName(String productName) {
+        return new SuccessDataResult<Product>(productDao.getByProductName(productName), "Data listelendi");
+    }
+
+    @Override
+    public DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId) {
+        return new SuccessDataResult<Product>(productDao.getByProductNameAndCategory_CategoryId(productName, categoryId), "Data listelendi");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId) {
+        return new SuccessDataResult<List<Product>>(productDao.getByProductNameOrCategory_CategoryId(productName, categoryId), "Data listelendi");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories) {
+        return new SuccessDataResult<List<Product>>(productDao.getByCategory_CategoryIdIn(categories), "Data listelendi");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameContains(String productName) {
+        return new SuccessDataResult<List<Product>>(productDao.getByProductNameContains(productName), "Data listelendi");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByProductNameStartsWith(String productName) {
+        return new SuccessDataResult<List<Product>>(productDao.getByProductNameStartsWith(productName), "Data listelendi");
+    }
+
+    @Override
+    public DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId) {
+        return new SuccessDataResult<List<Product>>(productDao.getByNameAndCategory(productName, categoryId), "Data listelendi");
     }
 }
